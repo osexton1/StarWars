33e51c53474b97f76133d592630baf644316359c
osexton1 <odhran.sexton@gmail.com>
2022-03-28 10:34:15 +0100

    Added initial character list file

---
+++ src/components/CharacterList.vue
@@ -0,0 +1,24 @@
+<template>
+    <tr>
+        <td>Luke</td>
+        <td>1.8m</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</template>
+
+<script>
+
+    export default {
+        name: 'CharacterList'
+    }
+</script>
+
+<style scoped>
+    td {
+        margin: 1em;
+        border: 2px solid black;
+    }
+</style>
