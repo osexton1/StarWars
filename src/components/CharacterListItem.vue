263e99eaf3a8df6e9d06785ec68525b8b7131b7d
osexton1 <odhran.sexton@gmail.com>
2022-03-28 12:04:11 +0100

    Created CharacterListItem and displayed the data

---
+++ src/components/CharacterListItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <tr>
+        <td>{{ character.name }}</td>
+        <td>{{ character.height }}</td>
+        <td>{{ character.mass }}</td>
+        <td>{{ character.created }}</td>
+        <td>{{ character.edited }}</td>
+        <td>{{ this.homeworld }}</td>
+    </tr>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'CharacterListItem',
+        data() {
+            return { homeworld: "" }
+        },
+        props: {
+            character: Object
+        },
+        methods: {
+            async getPlanetName() {
+                axios.get(this.character.homeworld).then(response => {
+                    this.homeworld = response.data.name;
+                });
+            }
+        },
+        created() {
+            this.getPlanetName()
+        }
+    }
+</script>
+
+<style scoped>
+    td {
+        margin: 1em;
+        border: 2px solid black;
+    }
+</style>
