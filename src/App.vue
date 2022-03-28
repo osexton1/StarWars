7f3440f35746d141381b74f5b7cfaaa3e159fb99
osexton1 <odhran.sexton@gmail.com>
2022-03-28 12:02:34 +0100

    Added api querying

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        Hello There
         <table>
             <tr>
                 <th>Name</th>
@@ -10,18 +9,34 @@
                 <th>Edited</th>
                 <th>Planet Name</th>
             </tr>
-            <CharacterList></CharacterList>
+            <CharacterList :characters="characters"></CharacterList>
         </table>
     </div>
 </template>
 
 <script>
     import CharacterList from './components/CharacterList';
+    import axios from 'axios';
+
+    const ROOT_URL = 'https://swapi.dev/api';
 
     export default {
         name: 'App',
         components: {
             CharacterList
+        },
+        data() {
+            return { characters: {} }
+        },
+        methods: {
+            async fetchCharacters() {
+                axios.get(`${ROOT_URL}/people`).then(response => {
+                    this.characters = response.data.results;
+                });
+            },
+        },
+        created() {
+            this.fetchCharacters();
         }
     }
 
