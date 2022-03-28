65d4d29eb03939949ca3ca4b5018366e7f9ac4d8
osexton1 <odhran.sexton@gmail.com>
2022-03-28 10:33:32 +0100

    Added base source files

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        Hello There
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Height</th>
+                <th>Mass</th>
+                <th>Created</th>
+                <th>Edited</th>
+                <th>Planet Name</th>
+            </tr>
+            <CharacterList></CharacterList>
+        </table>
+    </div>
+</template>
+
+<script>
+    import CharacterList from './components/CharacterList';
+
+    export default {
+        name: 'App',
+        components: {
+            CharacterList
+        }
+    }
+
+</script>
+
+<style scoped>
+    table {
+        margin: 1em;
+        border: 2px solid black;
+        border-collapse: collapse;
+        width: 80%;
+        text-align: center;
+    }
+
+    th {
+        margin: 1em;
+        border: 2px solid black;
+    }
+</style>
